f959ebabf9961ffc8d753819d41fb8eade08421a
daruan-rst <daruan.rst@gmail.com>
2023-11-15 17:36:26 -0300

    Person Repository

---
+++ bounty.hunter/src/main/java/wild/west/bounty/hunter/repositories/PersonRepository.java
@@ -0,0 +1,9 @@
+package wild.west.bounty.hunter.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import wild.west.bounty.hunter.model.Person;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
