713bff8bcebe5a3d1640efb6864376e9f01a0cbd
Daruan <daruan.rst@gmail.com>
2023-12-07 23:41:45 -0300

    Creating a town controller

---
+++ bounty.hunter/src/main/java/wild/west/bounty/hunter/controller/TownController.java
@@ -0,0 +1,88 @@
+package wild.west.bounty.hunter.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import wild.west.bounty.hunter.model.Person;
+import wild.west.bounty.hunter.model.Town;
+import wild.west.bounty.hunter.service.TownService;
+
+@AllArgsConstructor
+@RestController("/api/town/v1")
+@Tag(name = "Town", description = "Endpoints para a entidade Town")
+public class TownController {
+
+    private TownService service;
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Retrieves a Town by its id", description = "Com este endpoint, podemos retornar um Town com o seu Id",
+            tags = {"Town"},
+            responses = {
+                    @ApiResponse(description = "Sucess", responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = Person.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "BadRequest", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            })
+    public Town findTownById(@PathVariable Long id){
+        return service.findById(id);
+    }
+    @PostMapping
+    @Operation(summary = "Creates a Town", description = "Town",
+            tags = {"Town"},
+            responses = {
+                    @ApiResponse(description = "Sucess", responseCode = "200",
+                            content =
+                            @Content(schema = @Schema(implementation = Person.class))),
+                    @ApiResponse(description = "BadRequest", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            })
+    public Town createATown(@RequestBody Town town){
+        return service.createTown(town);
+    }
+
+    @PutMapping("/{id}")
+    @Operation(summary = "Updates a Town", description = "Town",
+            tags = {"Town"},
+            responses = {
+                    @ApiResponse(description = "Sucess", responseCode = "200",
+                            content =
+                            @Content(schema = @Schema(implementation = Person.class))),
+                    @ApiResponse(description = "BadRequest", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            })
+    public Town updateATown(@PathVariable Long id,
+                            @RequestBody Town town){
+        return service.updateTown(town, id);
+    }
+
+    @DeleteMapping
+    @Operation(summary = "Deletes a Town", description = "Deletes a Town",
+            tags = {"Town"},
+            responses = {
+                    @ApiResponse(description = "Sucess", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = Person.class))),
+                    @ApiResponse(description = "BadRequest", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            })
+    public Town deleteTown(Long id) {
+        return service.deleteTownById(id);
+    }
+}
--- bounty.hunter/src/main/java/wild/west/bounty/hunter/service/TownService.java
+++ bounty.hunter/src/main/java/wild/west/bounty/hunter/service/TownService.java
@@ -5,11 +5,15 @@ import lombok.extern.java.Log;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.stereotype.Service;
+import wild.west.bounty.hunter.controller.TownController;
 import wild.west.bounty.hunter.exceptions.ResourceNotFoundException;
 import wild.west.bounty.hunter.model.Town;
 import wild.west.bounty.hunter.repositories.TownRepository;
 
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Service
 @Log
@@ -27,14 +31,14 @@ public class TownService {
     public Town findById(Long id){
         log.info(String.format("Searching town by id: %s", id));
         Town town  = townRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No town found for this ID"));
-
+        town.add(linkTo(methodOn(TownController.class).findTownById(town.getId())).withSelfRel());
         return town;
     }
 
     public Town createTown(Town town){
         log.info("Creating a new town");
         town = townRepository.save(town);
-
+        town.add(linkTo(methodOn(TownController.class).createATown(town)).withSelfRel());
         return town;
     }
 
@@ -44,14 +48,15 @@ public class TownService {
         town.setSaloons(newTown.getSaloons());
         town.setTownName(newTown.getTownName());
         town = townRepository.save(town);
-
+        town.add(linkTo(methodOn(TownController.class).updateATown(town.getId(), town)).withSelfRel());
         return town;
     }
 
-    public void deleteTownById(Long id){
+    public Town deleteTownById(Long id){
         log.info("Deleting town by id");
         Town town = townRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No town found for this ID"));
-
         townRepository.deleteById(id);
+        town.add(linkTo(methodOn(TownController.class).deleteTown(town.getId())).withSelfRel());
+        return town;
     }
 }
