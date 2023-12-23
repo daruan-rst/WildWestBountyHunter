18bd1a3a760f1145975a0ac0fd90754b041f78bd
Daruan <daruan.rst@gmail.com>
2023-12-22 23:50:39 -0300

    creating EquipmentService

---
+++ bounty.hunter/src/main/java/wild/west/bounty/hunter/service/EquipmentService.java
@@ -0,0 +1,44 @@
+package wild.west.bounty.hunter.service;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.java.Log;
+import org.springframework.stereotype.Service;
+import wild.west.bounty.hunter.controller.EquipmentController;
+import wild.west.bounty.hunter.exceptions.ResourceNotFoundException;
+import wild.west.bounty.hunter.model.Equipment;
+import wild.west.bounty.hunter.model.Town;
+import wild.west.bounty.hunter.repositories.EquipmentRepository;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Service
+@AllArgsConstructor
+@Log
+public class EquipmentService {
+    
+    private final EquipmentRepository repository;
+
+    public Equipment findById(Long id){
+        log.info("Finding a equipment by id");
+        Equipment equipment = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No equipment found for this id"));
+//        equipment.add(linkTo(methodOn(EquipmentController.class).findEquipmentById(equipment.getId())).withSelfRel());
+        return equipment;
+    }
+    public Equipment createEquipment(Equipment equipment){
+        log.info("Creating a equipment");
+        equipment = repository.save(equipment);
+//        equipment.add(linkTo(methodOn(EquipmentController.class).createAEquipment(equipment)).withSelfRel());
+        return equipment;
+    }
+
+    public Equipment deleteEquipment(long id){
+        log.info("Deleting a equipment by id");
+        Equipment equipment = this.findById(id);
+//        equipment.removeLinks();
+//        equipment.add(linkTo(methodOn(EquipmentController.class).deleteEquipment(id)).withSelfRel());
+        repository.delete(equipment);
+        return equipment;
+    }
+    
+}
