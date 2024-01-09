ff639e0511552d62003fc78e05b8bbef99771152
Daruan <daruan.rst@gmail.com>
2024-01-08 22:29:37 -0300

    overriding method to configure content negotiation

--- bounty.hunter/src/main/java/wild/west/bounty/hunter/config/WebConfig.java
+++ bounty.hunter/src/main/java/wild/west/bounty/hunter/config/WebConfig.java
@@ -2,7 +2,9 @@ package wild.west.bounty.hunter.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -26,5 +28,17 @@ public class WebConfig implements WebMvcConfigurer {
         @Override
         public void extendMessageConverters(List<HttpMessageConverter<?>> converters){
 //            converters.add()
+        }
+
+        @Override
+        public void configureContentNegotiation(ContentNegotiationConfigurer configurer){
+            configurer.favorParameter(false)
+                    .ignoreAcceptHeader(false)
+                    .useRegisteredExtensionsOnly(false)
+                    .defaultContentType(MediaType.APPLICATION_JSON);
+            //TODO: aceitar xml e yml
+//                    .mediaType("json", MediaType.APPLICATION_JSON)
+//                    .mediaType("xml", MediaType.APPLICATION_XML)
+//                    .mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML);
         }
 }
