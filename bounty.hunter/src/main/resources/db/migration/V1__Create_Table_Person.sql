1d75f42eaca3eba478ff2f511bb87da68cdb5b7a
Daruan <daruan.rst@gmail.com>
2024-03-10 20:45:12 -0300

    fix na  migratrion de criar tabela de persons

--- bounty.hunter/src/main/resources/db/migration/V1__Create_Table_Person.sql
+++ bounty.hunter/src/main/resources/db/migration/V1__Create_Table_Person.sql
@@ -4,5 +4,5 @@ CREATE TABLE IF NOT EXISTS "person" (
   "name" varchar(80) NOT NULL,
   "town_id" SERIAL NOT NULL,
   "money" varchar(100) NOT NULL,
-  "reputation" varchar(100) NOT NULL
+  "reputation" varchar(100)
 );
