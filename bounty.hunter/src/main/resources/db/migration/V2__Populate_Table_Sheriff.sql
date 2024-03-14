7080a02e4be454809268d31cde08922a4939143f
Daruan <daruan.rst@gmail.com>
2024-03-13 23:56:04 -0300

    Migration de inserir Sheriff na tabela de person

---
+++ bounty.hunter/src/main/resources/db/migration/V2__Populate_Table_Sheriff.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO "person" ("_OBJECT_TYPE", "name", "town_id", "money", "reputation")
+VALUES
+    ('SHERIFF', 'Carl Sagan', 1, '1000', NULL),
+    ('SHERIFF', 'Pete Townshend', 2, '2050', NULL),
+    ('SHERIFF', 'David Guilmour', 3, '8000', NULL),
+    ('SHERIFF', 'Marvin Gaye', 4, '1800', NULL);
