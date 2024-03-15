d51b0c66eb0cf7ceff8eb02d375bcd828f2614b7
Daruan <daruan.rst@gmail.com>
2024-03-15 20:59:43 -0300

    Migration para popular table de person

---
+++ bounty.hunter/src/main/resources/db/migration/V4__Populate_Table_Outlaw.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO "person" ("_OBJECT_TYPE", "name", "town_id", "money", "reputation")
+VALUES
+    ('OUTLAW', 'Maynard James Keenan', 4, '500', NULL),
+    ('OUTLAW', 'Adam Jones', 1, '1250', NULL),
+    ('OUTLAW', 'Danny Carey', 2, '8000', NULL),
+    ('OUTLAW', 'Justin Chancellor', 3, '2900', NULL);
