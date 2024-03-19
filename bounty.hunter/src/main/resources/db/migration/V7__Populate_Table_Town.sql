3fc3b43d50b494394a1c3fd3b1ab0ca24661e971
Daruan <daruan.rst@gmail.com>
2024-03-18 23:37:14 -0300

    Migration para popular table de town

---
+++ bounty.hunter/src/main/resources/db/migration/V7__Populate_Table_Town.sql
@@ -0,0 +1,11 @@
+INSERT INTO town (name) VALUES 
+    ('Deadwood'),
+    ('Tombstone'),
+    ('Dodge City'),
+    ('Tucson'),
+    ('Cheyenne'),
+    ('Durango'),
+    ('Santa Fe'),
+    ('Laramie'),
+    ('Virginia City'),
+    ('Silver City');
