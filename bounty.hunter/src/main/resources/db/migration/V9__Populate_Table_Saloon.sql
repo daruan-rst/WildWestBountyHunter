d019a0c28cd1e29d44f59b540e17710c12c116cf
Daruan <daruan.rst@gmail.com>
2024-03-22 23:40:47 -0300

    Migration para popular table de saloon

---
+++ bounty.hunter/src/main/resources/db/migration/V9__Populate_Table_Saloon.sql
@@ -0,0 +1,11 @@
+INSERT INTO saloon (saloon_name, town_id) VALUES
+    ('The Last Chance Saloon', 1),      -- Deadwood
+    ('The Bird Cage Theatre', 2),       -- Tombstone
+    ('Long Branch Saloon', 3),          -- Dodge City
+    ('The Palace Saloon', 4),           -- Tucson
+    ('The Plains Hotel Saloon', 5),     -- Cheyenne
+    ('The Strater Hotel Saloon', 6),    -- Durango
+    ('La Fonda on the Plaza', 7),       -- Santa Fe
+    ('The Buckhorn Exchange', 8),       -- Laramie
+    ('The Bucket of Blood Saloon', 9),  -- Virginia City
+    ('The Palace Hotel Saloon', 10);    -- Silver City
