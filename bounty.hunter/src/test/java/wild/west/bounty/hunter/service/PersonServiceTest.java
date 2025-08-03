0854c4f29ab73ee72abfe1120be439834dd3f5ca
Daruan <daruan.rst@gmail.com>
2025-08-03 19:46:09 -0300

    Tests for creating a BountyHunter

--- bounty.hunter/src/test/java/wild/west/bounty/hunter/service/PersonServiceTest.java
+++ bounty.hunter/src/test/java/wild/west/bounty/hunter/service/PersonServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import wild.west.bounty.hunter.model.BountyHunter;
 import wild.west.bounty.hunter.model.Citizen;
 import wild.west.bounty.hunter.model.Person;
 import wild.west.bounty.hunter.repositories.PersonRepository;
@@ -14,6 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static wild.west.bounty.hunter.model.enums.Reputation.CRUEL;
 
 @ExtendWith(MockitoExtension.class)
 class PersonServiceTest {
@@ -40,5 +42,26 @@ class PersonServiceTest {
         // Assert
         assertTrue(citizen.isAlive());
         assertInstanceOf(Citizen.class, citizen);
+    }
+
+    @Test
+    void createBountyHunter_shouldSetAliveToTrue() {
+        someone = new BountyHunter();
+        someone.setId(1L);
+        someone.setName("John Wayne");
+        someone.setAlive(false);
+
+        BountyHunter hunter = (BountyHunter) someone;
+
+        hunter.setReputation(CRUEL);
+
+        when(personRepository.save(any(Person.class))).thenReturn(hunter);
+
+        // Act
+        Person citizen = personService.createPerson(hunter);
+
+        // Assert
+        assertTrue(citizen.isAlive());
+        assertInstanceOf(BountyHunter.class, citizen);
     }
 }
